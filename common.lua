8a72816e4e1dc00eb6457c1482cbe4e3f574a466
r0fls <raphael.deem@gmail.com>
2016-02-06 17:05:51 -0800

    median function

--- common.lua
+++ common.lua
@@ -6,6 +6,10 @@ math.randomseed(os.clock()*10^20/729351)
 -- on some OSs the first number may not be random
 math.random()
 
+-- each distribution implements this
+function stats.random(dist)
+    return dist.quantile(dist, math.random())
+end
 
 function stats.average(data)
     return stats.sum(data)/#data
@@ -19,8 +23,13 @@ function stats.sum(data)
     return total
 end
 
-function stats.random(dist)
-    return dist.quantile(dist, math.random())
+function stats.median(data)
+    table.sort(data)
+    if #data % 2 == 1 then
+        return data[math.ceil(#data/2)] 
+    else
+        return (data[#data/2]+data[1+(#data/2)])/2
+    end
 end
 
 return stats
--- test.lua
+++ test.lua
@@ -4,8 +4,9 @@ local i = bernoulli.new(.5)
 print(i:pmf(1)) --> .5
 --print(i:quantile(.4)) --> 0
 print(i:random())
-local array = {1,2,3,4}
+local array = {4,2,3,1}
 print(stats.average(array)) --> 2.5
+print(stats.median(array)) --> 2.5
 print(stats.sum(array)) --> 10
 local bernoulli_data = {1,1,1,0}
 local b = bernoulli.fit(bernoulli_data)
