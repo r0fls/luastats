62cfc9462c5cf7df811b468ccab5b8637a020f6f
Raphael Deem <raphael.deem@gmail.com>
2016-02-08 19:37:51 -0800

    Update stats.lua

--- stats.lua
+++ stats.lua
@@ -3,4 +3,6 @@ stats.__index = stats
 stats['bernoulli']=require("distributions/bernoulli")
 stats['laplace']=require("distributions/laplace")
 stats['exponential']=require("distributions/exponential")
+stats['uniform']=require("distributions/uniform")
+stats['common']=require("common")
 return stats
